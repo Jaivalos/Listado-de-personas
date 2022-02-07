fd3a8b36ccbc138368619c282e57860889d546f5
Javier Avalos <javalos18j2@gmail.com>
2022-02-06 20:03:50 -0600

    Creacion de la funcion listado de personas

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,7 +1,16 @@
 
 const personas = [
-    p1 = new Persona("Juan", "Perez"),
-    p2 = new Persona("Julia", "Saenz"),
-]
+    new Persona("Juan", "Perez"),
+    new Persona("Julia", "Saenz"),
+];
+
+const mostrarPersonas = () =>{
+    let texto = "";
+    for (const persona of personas) {
+        texto += `<li> ${persona.nombre} ${persona.apellido} </li>`;
+    }
+    let lista = document.getElementById("persona");
+    lista.innerHTML = texto;
+}
 
 
